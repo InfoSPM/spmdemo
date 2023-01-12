d31f8845393082c492a1ad4112c80f35ccd66664
SPM Global Technologies <118279637+InfoSPM@users.noreply.github.com>
2023-01-12 10:56:11 +0530

    remove a straight line

--- components/Common/PageSeo.js
+++ components/Common/PageSeo.js
@@ -3,7 +3,7 @@ import { NextSeo } from 'next-seo';
 const PageSeo = ({ seo, pageName }) => {
     const seoData = seo?.data?.length > 0 && seo?.data[0] && seo?.data[0].attributes?.seo ? seo?.data[0].attributes?.seo : {};
     return <NextSeo
-        title={seoData?.metaTitle || `${pageName} | SPM Global Technologies | Simplifying digitalisation - for you and your clients`}
+        title={seoData?.metaTitle || `${pageName} | SPM Global Technologies  `}
         description={seoData?.metaDescription}
         canonical={seoData?.canonicalURL}
         additionalMetaTags={[{
